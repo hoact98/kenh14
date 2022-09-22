5d970d7c6738974673139bca91fa4cfe5669d299
Hoa98 <chuthihoa98bgg@gmail.com>
2022-09-22 07:11:30 +0700

    regex

--- src/app/modules/post/components/post-content/post-content.component.ts
+++ src/app/modules/post/components/post-content/post-content.component.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
 export class PostContentComponent implements OnInit {
 
   constructor(private titleService:Title, private route: ActivatedRoute) { }
-
+  id:any;
   slug: any;
   title: any;
   content: any;
@@ -20,12 +20,14 @@ export class PostContentComponent implements OnInit {
       this.voice = false;
      this.route.params.subscribe(params => {
       this.slug = params['slug'];
-      let regex = /\d+/g;
-      let matches = params['slug'].match(regex);
-      alert(matches)
-      console.log(matches.toString());
+      // let regex = /\d+/g;
+      let regex = /(\/[a-zA-Z0-9_\-.]+)*?\d+.chn{1}$/ig;
+      let matches = params['slug'].match(regex).toString();
+      // alert(matches)
+      this.id = matches.replace('.chn', '')
+      console.log(this.id);
       // console.log(matches);
-      let post = environment.data.filter(element => element.slug == this.slug)[0];
+      let post = environment.data.filter(element => element.id == this.id)[0];
       this.title = post.title;
       this.content = post.content;
       this.titleService.setTitle(this.title);
