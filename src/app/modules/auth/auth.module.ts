baee9335fec590fccba0bf3b1a9ab749aab08fb3
unknown <hoact98bg@gmail.com>
2022-10-14 17:43:34 +0700

    update

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { AuthGuard } from 'src/app/_helpers/auth.guard';
 import { AuthLoginComponent } from './components/login/login.component';
 import { AuthLogoutComponent } from './components/logout/logout.component';
 import { AuthRegisterComponent } from './components/register/register.component';
 import { CommonModule } from '@angular/common';
+import { HttpLoaderFactory } from 'src/app/app.module';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
@@ -24,6 +27,15 @@ export const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
+    // ngx-translate and the loader module
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    }),
   ],
   exports: [RouterModule],
 })
