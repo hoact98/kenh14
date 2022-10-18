222827a1ea2a2555b32b5070b23af4f69415083a
unknown <hoact98bg@gmail.com>
2022-10-18 18:02:37 +0700

    update

--- src/app/components/_layout/_administrator/site-header/site-header.component.ts
+++ src/app/components/_layout/_administrator/site-header/site-header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-administrator-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.scss']
 })
 export class AdministratorSiteHeaderComponent implements OnInit {
-
-  constructor() { }
+  language: string = 'vi';
+  constructor(private translate: TranslateService) {
+  // language
+    translate.setDefaultLang('vi');
+    translate.use('vi')
+  }
 
   ngOnInit() {
   }
-
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    this.language = language;
+  }
 }
